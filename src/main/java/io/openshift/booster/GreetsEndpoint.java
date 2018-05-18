b573b3e542f95300e852c7df7c467146c9555dca
Andreas Hess <andreas.h.hess@googlemail.com>
2018-05-18 11:47:11 +0000

    new Endpoint for 1. cahnge  test

--- src/main/java/io/openshift/booster/GreetsEndpoint.java
+++ src/main/java/io/openshift/booster/GreetsEndpoint.java
@@ -4,12 +4,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 
 @Path("/greets")
 public class GreetsEndpoint {
 
-    private static final String template = "Hello, %s!";
 
     @GET
     @Path("/{id:[0-9][0-9]*}")
